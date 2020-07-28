ad6309ebaecbb666da92cd0a58800fde0baa3b31
toothless <52105661+kimDaeGeon@users.noreply.github.com>
2020-07-28 17:53:27 +0900

    0728

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function Search() {
     var SummonerName = document.getElementById("summoner-name").value;
     location.href="search/search_result.html?" + "summoner_name=" + SummonerName;
-    return SummonerName;
 }
--- search/request.js
+++ search/request.js
@@ -1,30 +1,57 @@
 /// get querry string
-
-var SummonerName = "";
-var API_KEY = "RGAPI-29ae83fb-99e1-4698-ab07-fd3fb2e7b9fc";
+var API_KEY = "RGAPI-63383576-64d8-4b63-9c74-004b146caeff";
+var URL_summoner = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
 
 window.onload = function() {
     var querryString = "summoner_name=";
     var currentURL = window.location.href;
 
-    document.getElementById("page_info").innerHTML = currentURL;
-    SummonerName = currentURL.slice(currentURL.indexOf("summoner_name=") + querryString.length);
+    var SummonerName = currentURL.slice(currentURL.indexOf("summoner_name=") + querryString.length);
 
-    GetRequest();
+    URL_summoner += SummonerName;
+    URL_summoner += "?api_key=" + API_KEY;
+    console.log(URL_summoner);
+
+    // Get Request
+
+    var request = new Request(URL_summoner, {method:'GET', mode:'no-cors'});
+
+
+/*
+    fetch(URL_summoner)
+        .then(response => response.json())
+        .then((out) => {
+            console.log("checkout this json! ", out);
+        })
+        .catch(err => {throw err});
+
+    */
+   getJSON(URL_summoner, function(err, data){
+       if(err != null) alert("something went wrong: " + err);
+       console.log(data);
+
+
+       // json
+       document.getElementById("summoner-name").innerHTML = data.name;
+
+       document.getElementById("summoner-level").innerHTML = "Level : " + data.summonerLevel;
+
+   });
+   
+
 }
 
-
-function GetRequest(){
-    /// get request
-    var URL = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ SummonerName + "?api_key=" + API_KEY;
-
-    document.getElementById("page_info").innerHTML = URL;
-
+var getJSON = function(url, callback){
     var request = new XMLHttpRequest();
-
-    request.open('GET', URL, true);
-    request.send(null);
-
-    document.write(request.responseText);
-
-}
+    request.open('GET', URL_summoner, true);
+    request.responseType = "json";
+    request.onload = function() {
+        if(request.status == 200){
+            callback(null, request.response);
+        }
+        else{
+            callback(status, request.response);
+        }
+    };
+    request.send();
+};
